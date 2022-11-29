d54701e48982f602f4f32f28ea2daaedb21d4a26
emregulistan <emregulistan1@gmail.com>
2022-11-29 22:56:30 +0300

    components & databinding operations

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 import { Recipe } from '../recipe.model'
 
@@ -8,10 +8,15 @@ import { Recipe } from '../recipe.model'
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is simply a test','https://st3.depositphotos.com/7691758/18074/i/600/depositphotos_180742044-stock-photo-fried-veal-liver-slices-in.jpg'),
-    new Recipe('A Test Recipe', 'This is simply a test','https://st3.depositphotos.com/7691758/18074/i/600/depositphotos_180742044-stock-photo-fried-veal-liver-slices-in.jpg')
+    new Recipe('Another Test Recipe', 'This is simply a test','https://st3.depositphotos.com/7691758/18074/i/600/depositphotos_180742044-stock-photo-fried-veal-liver-slices-in.jpg')
 
   ];
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);  
+  }
 
 }
